0ac19c92a8a8b9fbbf20b7f2576a09627250cf86
Natatashkin <cs070283sng@gmail.com>
2021-10-18 01:47:10 +0300

    add files & task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.js-gallery');
+gallery.addEventListener('click', onModalOpen);
+
+function createGalleryItems(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt='${description}'
+          />
+        </a>
+      </div>`,
+    )
+    .join('');
+}
+const galleryImages = createGalleryItems(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryImages);
+
+function onModalOpen(e) {
+  e.preventDefault();
+
+  const url = getImageUrl(e.target);
+
+  const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">`);
+
+  instance.show(() => {
+    window.addEventListener('keydown', onEscPress);
+  });
+
+  function onEscPress(e) {
+    console.log(e.key);
+    if (e.key === 'Escape') {
+      instance.close(() => {
+        window.removeEventListener('keydown', onEscPress);
+      });
+    }
+  }
+}
+
+function getImageUrl(image) {
+  return image.dataset.source;
+}
